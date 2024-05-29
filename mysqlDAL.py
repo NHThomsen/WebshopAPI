5bac115eb51572025b0764b70b81859fc87a7688
nhthomsen <nooblas@hotmail.com>
2024-05-29 11:56:50 +0200

    Removed unused variable

--- mysqlDAL.py
+++ mysqlDAL.py
@@ -2,14 +2,12 @@ import mysql.connector
 import product
 
 class MySQLDAL:
-    mydb = None
-
     def __init__(self):
         self.mydb = mysql.connector.connect(host="localhost",user="root",password="Pa$$w0rd")
     
     def GetAllProducts(self):
         mycursor = self.mydb.cursor()
-        mycursor.execute("SELECT * FROM webshop.Products")
+        mycursor.execute("CALL `webshop`.`GetAllProducts`()")
         myresult = mycursor.fetchall()
         products = []
         for prod in myresult:
